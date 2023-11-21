d05162a41c10f48a54969e53901542d66567beb1
romanstukalkin <stukalkinrv@yandex.ru>
2023-11-21 16:53:24 +0400

    1 commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите количество элементов массива:");
+int count_in_array = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[count_in_array];
+
+for (int i = 0; i < count_in_array; i++)
+{
+    Console.Write($"Введите {i+1} элемент массива:");
+    array[i] = Console.ReadLine();
+}
+
+
+
+/*for (int i = 0; i < count_in_array; i++)
+{
+    Console.Write($"{array[i]} ");
+}*/
